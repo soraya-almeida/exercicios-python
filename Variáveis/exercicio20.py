9bc0140b084292389519f293e1dfb2b8ad1a37cc
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 20:06:44 -0300

    feito

---
+++ Variáveis/exercicio20.py
@@ -0,0 +1,7 @@
+#Peça um número float e exiba apenas a parte inteira dele.
+
+num = float(input("Informe um número quebrado: "))
+
+num_int = int(num)
+
+print("Aqui está o número quebrado, só que INTEIRO: ", num_int)
