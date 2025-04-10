5415eb6bf9576bca0f5ca1a288dc9da669fb5e37
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 19:22:40 -0300

    feito

---
+++ Variáveis/exercicio12.py
@@ -0,0 +1,8 @@
+#Peça dois números float e mostre a média aritmética entre eles.
+
+num1 = float(input("Informe um numero: "))
+num2 = float(input("Informe outro número: "))
+
+media = (num1 + num2)/2
+
+print("A média é", media)
