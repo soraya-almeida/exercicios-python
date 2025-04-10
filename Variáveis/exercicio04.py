849220912628e0044647d26f72b10e1dde953618
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:12:33 -0300

    quarta feita

---
+++ Variáveis/exercicio04.py
@@ -0,0 +1,6 @@
+#Peça o nome e a idade do usuário e exiba uma frase completa com essas informações.
+
+nome = str(input("Informe o seu nome: "))
+idade = int(input("Informe sua idade: "))
+
+print("Você se chama", nome, "e tem", idade,"anos!")
