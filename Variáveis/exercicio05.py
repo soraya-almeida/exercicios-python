39972eed512d98223378e0b133e758dae20902ea
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:17:23 -0300

    quinta feita

---
+++ Variáveis/exercicio05.py
@@ -0,0 +1,8 @@
+#Verifique se uma pessoa é maior de idade, com base na idade informada.
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= 18:
+    print("Você é MAIOR de idade!")
+else:
+    print("Você é MENOR de idade!")
