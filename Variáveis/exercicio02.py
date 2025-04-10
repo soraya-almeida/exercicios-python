05ce593f70656898c93db7bbb014136d4ddae7ac
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:04:32 -0300

    Segundo feito

---
+++ Variáveis/exercicio02.py
@@ -0,0 +1,8 @@
+#Solicite dois números inteiros, some-os e exiba o resultado.
+
+Valor1 = int(input("Informe um valor: "))
+Valor2 = int(input("Informe outro valor: "))
+
+Soma = Valor1 + Valor2
+
+print("A soma dos dois valores:", Soma)
