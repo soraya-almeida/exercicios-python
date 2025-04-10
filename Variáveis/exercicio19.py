af80f0db7280f524dfeab6769777bf117fbd522d
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 20:03:14 -0300

    feito

---
+++ Variáveis/exercicio19.py
@@ -0,0 +1,12 @@
+#Receba dois valores e troque os valores entre as variáveis, depois mostre o resultado.
+
+valor1 = int(input("Informe o primeiro valor: "))
+valor2 = int(input("Informe o segundo valor: "))
+
+valor_aux = valor1
+valor1 = valor2
+valor2 = valor_aux
+
+print(valor1)
+print(valor2)
+
