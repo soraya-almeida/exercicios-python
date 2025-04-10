a904efe8a3a5e3631253e1c4b8355862a1b577cf
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 19:42:45 -0300

    feito

---
+++ Variáveis/exercicio16.py
@@ -0,0 +1,7 @@
+#Peça o nome e o curso de um aluno e exiba uma frase como: "Fulano está matriculado no curso de X".
+
+nome = str(input("Informe seu nome: "))
+curso = str(input("Informe seu curso: "))
+
+print(nome, "está matriculado(a) no curso", curso)
+
