947977f713f6d1094913dd344d7cf0e5b14f8a1e
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:53:42 -0300

    Create exercicio07.py

---
+++ Variáveis/exercicio07.py
@@ -0,0 +1,7 @@
+#Calcule a área de um círculo, pedindo o raio (float) e usando a fórmula: área = π * raio².
+
+raio = float(input("Informe o raio do circulo: "))
+
+area = 3.14159 * (raio ** 2)
+
+print("A área do circulo com base no seu raio informado é", area)
