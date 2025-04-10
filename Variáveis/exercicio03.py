91e49ae89dcea8088514a95ef87ce7735e91e4ed
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:08:56 -0300

    terceira feita

---
+++ Variáveis/exercicio03.py
@@ -0,0 +1,9 @@
+#Peça ao usuário três notas (float), calcule a média e exiba.
+
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+nota3 = float(input("Informe a terceira nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print("A sua média é", media)
