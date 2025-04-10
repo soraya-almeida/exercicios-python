ca6097ccb6913c10d93c4b52a688b351e1c4562f
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 18:36:46 -0300

    feito a 10

---
+++ Variáveis/exercicio10.py
@@ -0,0 +1,8 @@
+#Peça uma senha ao usuário e retorne True se for igual a "python123", senão False.
+
+senha = str(input("Informe a senha: "))
+
+if senha == "python123" :
+    print("Você acertou a senha!")
+else:
+    print("Você errou a senha, tente novamente!")
