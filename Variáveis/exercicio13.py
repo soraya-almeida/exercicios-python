521d609b774f65f2fba8ee5365d428649dba9e33
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 19:26:00 -0300

    feito

---
+++ Variáveis/exercicio13.py
@@ -0,0 +1,6 @@
+#Peça o nome de um produto e o seu preço (float), depois mostre uma frase com os dois.
+
+produto = str(input("Informe o nome do produto: "))
+preco = float(input("Informe o valor do produto: "))
+
+print("O seu produto é", produto, "e o preço é", preco, ".")
