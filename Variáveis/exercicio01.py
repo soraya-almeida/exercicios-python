b44a5b4e444bb8c53a5443aaed711d72c652ba55
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:00:07 -0300

    Primeira feita

---
+++ Variáveis/exercicio01.py
@@ -0,0 +1,6 @@
+#Crie quatro variáveis, uma de cada tipo: inteiro, float, string e booleano.
+
+nome = "Soraya Almeida" #string
+idade = 18              #inteiro
+altura = 1.65           #float
+rica = True             #booleano
