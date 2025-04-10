87500e40edc0c956d967fa971e54f2712269f6b0
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 19:47:48 -0300

    feito

---
+++ Variáveis/exercicio17.py
@@ -0,0 +1,9 @@
+#Peça ao usuário dois números e diga qual é o maior.
+
+num1 = int(input("Informe um valor: "))
+num2 = int(input("Informe outro valor: "))
+
+if num1 > num2:
+    print("O primeiro valor é maior que o segundo valor!")
+else:
+    ("O segundo valor é maior que o segundo valor!")
