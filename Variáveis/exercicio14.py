0151562f453416d3030b5b8b05d7c2708a00ca39
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 19:34:36 -0300

    feito

---
+++ Variáveis/exercicio14.py
@@ -0,0 +1,8 @@
+#Receba um número inteiro e diga se ele é múltiplo de 5 (retorne True ou False).
+
+num = int(input("Informe um número: "))
+
+if num % 5 == 0:
+    print("Esse número é multiplo de 5!")
+else:
+    print("Esse número não é multiplo de cinco!")
