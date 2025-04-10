64dd4ecf2a39391f1095f9960bec370693c9779f
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 12:56:57 -0300

    Create exercicio08.py

---
+++ Variáveis/exercicio08.py
@@ -0,0 +1,8 @@
+#Peça dois números float e mostre o resultado da divisão entre eles.
+
+Valor1 = float(input("Informe um número: "))
+Valor2 = float(input("Informe outro número: "))
+
+divisao = Valor1 / Valor2
+
+print("Aqui está a divisão dos dois valores informados:", divisao)
