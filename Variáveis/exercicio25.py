2fb7f9de3f875566d982407ff056f41f8238f94f
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-10 23:09:12 -0300

    feito

---
+++ Variáveis/exercicio25.py
@@ -0,0 +1,8 @@
+#Peça um nome completo e exiba ele todo em letras maiúsculas.
+nome_completo = str(input("Informe seu nome completo: "))
+
+maiuscula = nome_completo.upper()
+minuscula = nome_completo.lower()
+
+print("Aqui está seu nome MAIUSCULO: ", maiuscula)
+print("Aqui está seu nome MINUSCULO: ", minuscula)
