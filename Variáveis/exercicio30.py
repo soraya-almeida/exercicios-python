9cf34550ca3c26defb133cb3c10757d84648fe08
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-11 19:09:41 -0300

    feito

---
+++ Variáveis/exercicio30.py
@@ -0,0 +1,7 @@
+#Peça ao usuário para digitar "sim" ou "não", e transforme isso em um valor booleano (True para "sim", False para "não").
+
+palavra = str(input("Informe sim ou não: "))
+
+resultado = palavra == "sim"
+
+print(resultado)
