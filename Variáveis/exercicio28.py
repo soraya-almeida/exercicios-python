f8305f8e8de1c031f06e7b2ce6bb2c8c66085fff
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-11 18:48:47 -0300

    feito

---
+++ Variáveis/exercicio28.py
@@ -0,0 +1,7 @@
+#Peça um número e diga se ele é decimal (separado por ponto) ou inteiro (use float() e is_integer()).
+
+Valor = float(input("Informe um número: "))
+
+tipo = type(Valor)
+
+print(tipo)
---
+++ Variáveis/exercicio29.py
@@ -0,0 +1,6 @@
+#Peça um número e um texto, e mostre os dois juntos em uma frase como: "Você digitou o número X e a palavra Y".
+
+num = int(input("Informe um número: "))
+car = str(input("Informe uma palavras: "))
+
+print("Você digitou o número", num, "e a palavras", car)
