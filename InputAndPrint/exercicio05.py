e65e089ac0221a83dc5a9d4a092e10b73b4dc36d
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-12 12:07:24 -0300

    feito

---
+++ InputAndPrint/exercicio05.py
@@ -0,0 +1,8 @@
+#Peça nome, idade e cidade, e imprima:
+#"[nome] tem [idade] anos e mora em [cidade]."
+
+nome = input("Informe seu nome: ")
+idade = input("informe sua idade: ")
+cidade = input("Informe sua cidade: ")
+
+print(f"O seu nome é {nome}, tem {idade} anos e mora em {cidade}.")
