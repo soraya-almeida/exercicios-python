f41af1d6d071ed46df82aeb933aa9d7cc98daf84
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-12 11:58:27 -0300

    feito

--- InputAndPrint/exercicio03.py
+++ InputAndPrint/exercicio03.py
@@ -1,1 +1,6 @@
-#
+#Peça duas palavras e imprima elas na mesma linha, separadas por hífen.
+
+palavra1 = str(input("Informe uma palavra: "))
+palavra2 = str(input("Informe outra palavra: "))
+
+print(palavra1 ,"-", palavra2)
