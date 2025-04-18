92d68b0aa9fd76da7dd67d428b1c44abbc26d118
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-17 22:25:24 -0300

    feito

--- for/exercicio02.py
+++ for/exercicio02.py
@@ -1,2 +1,7 @@
 #Conte quantas letras tem cada palavra:
 #Pra cada palavra da lista, mostre quantas letras ela tem.
+
+palavras = ["objeto", "casa", "cadeira"]
+
+for palavra in palavras:
+    print("A palavra", palavra, "contém", len(palavra), "letras!")
---
+++ for/exercicio03.py
@@ -0,0 +1,10 @@
+#Mostre as palavras em letras maiúsculas:
+#Pegue a lista e transforme cada palavra em letras grandes.
+
+#upper() - MAIUSCULA
+#lower() - MINUSCULA
+
+personagens = ["Naruto", "Sasuke", "Sakura", "Kakashi"]
+
+for personagem in personagens:
+    print("Personagem", personagem, "-", personagem.upper())
