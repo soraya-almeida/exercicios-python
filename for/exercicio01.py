b40a163ca00789f3bb8b79015e2a688cd582319d
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-17 22:13:25 -0300

    feito

--- for/exercicio01.py
+++ for/exercicio01.py
@@ -1,7 +1,7 @@
-#Contar de 1 a 10:
-#Faça um programa que exiba os números de 1 a 10, um por linha.
+#Imprima cada palavra da lista:
+#Tem uma lista com palavras como ["cachorro", "gato", "pássaro"]. Mostre uma por uma.
 
-num = 1
-#for variavel in algo:
-for num in range(10):
-    print(num)
+animais = ["cachorro", "gato", "pássaro"]
+
+for animal in animais:
+    print("Eu gosto de", animal)
--- for/exercicio02.py
+++ for/exercicio02.py
@@ -1,1 +1,2 @@
-#
+#Conte quantas letras tem cada palavra:
+#Pra cada palavra da lista, mostre quantas letras ela tem.
