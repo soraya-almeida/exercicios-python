9fb453ba809c274981ea1c30de6a4aee4e1fcabe
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-17 22:42:19 -0300

    feito

--- for/exercicio05.py
+++ for/exercicio05.py
@@ -1,1 +1,11 @@
-#
+#Verifique se a palavra tem a letra "a":
+#Pra cada palavra, diga se ela tem a letra "a" dentro.
+
+#len() - quantidade dem numero
+#count() - contagem de palavras ou letras especificas
+
+colors = ["Yellow", "Blue", "Red", "Green", "Black"]
+
+for color in colors:
+    contagem = color.count("a")
+    print("A palavra", color, "tem", contagem), "letras A!"
