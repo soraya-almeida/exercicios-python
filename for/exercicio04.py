2c29de4396c96b41ea3907c158ad71829584c5fa
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-17 22:31:53 -0300

    feito

--- for/exercicio04.py
+++ for/exercicio04.py
@@ -1,1 +1,7 @@
-#
+#Adicione um emoji no final de cada palavra:
+#Tipo: "cachorro 🐶", "gato 🐱"...
+
+sentimentos = ["Tristeza☹️", "Alegria😂", "Raiva👿"]
+
+for sentimento in sentimentos:
+    print("Sentimento de", sentimento)
