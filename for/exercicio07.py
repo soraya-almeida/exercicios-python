f774bf95076849d5b4d2f70d9a55bbeb8d34382b
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-17 23:07:00 -0300

    ainda incompleto

---
+++ for/exercicio07.py
@@ -0,0 +1,7 @@
+#Junte todas as palavras com hífen entre elas:
+#Transforme ["sol", "lua", "estrela"] em "sol-lua-estrela" (dica: dá pra fazer isso com for também!).
+
+palavras = ["sol-lua-estrela"]
+
+for palavra in palavras:
+    print(palavra)
---
+++ for/exercicio08.py
@@ -0,0 +1,8 @@
+#Conte quantas palavras começam com a letra "b":
+#Tipo: "bola", "bicho"...
+
+palavras = ["bicho", "bola", "banco"]
+
+for palavra in palavras:
+    if palavra.count("b") == "b":
+        print(palavra)
