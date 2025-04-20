b128f4005f6ed8d2c6ea86929715f690a84cd908
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-20 19:14:35 -0300

    feito

---
+++ while/exercicio01.py
@@ -0,0 +1,8 @@
+#Contagem de 1 a 10
+#Escreva um programa que use while para imprimir os números de 1 até 10.
+
+contador = 1
+
+while contador <=10:
+    print(f"contando...{contador}")
+    contador += 1
