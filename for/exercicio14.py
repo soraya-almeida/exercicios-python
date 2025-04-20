d0f1c03834418208524fb2aabfdadc9969850338
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-19 23:34:38 -0300

    feito

---
+++ for/exercicio14.py
@@ -0,0 +1,4 @@
+#Repita a frase “Eu consigo!” 4 vezes.
+
+for _ in range(4):
+    print("Eu consigo")
---
+++ for/exercicio15.py
@@ -0,0 +1,12 @@
+#Imprima os números de 1 a 5, mas cada um repetido 3 vezes.
+
+for _ in range(3):
+    print("1")
+for _ in range(3):
+    print("2")
+for _ in range(3):
+    print("3")
+for _ in range(3):
+    print("4")
+for _ in range(3):
+    print("5")
