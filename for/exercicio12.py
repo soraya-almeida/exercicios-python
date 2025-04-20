40383ff06a14d9c7e2ed0cc200abfc0d283c9ce3
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-19 23:20:42 -0300

    feito

---
+++ for/exercicio12.py
@@ -0,0 +1,7 @@
+#Conte de 0 até 20 pulando de 2 em 2 (somente usando range).
+
+for numero in range(0, 20, 2):
+    print(numero)
+
+#o primeiro valor dentro do range, determina o começo, o do meio indica o final
+#e o ultimo indica de quanto em quanto vc quer pular.
---
+++ for/exercicio13.py
@@ -0,0 +1,4 @@
+#Mostre uma contagem regressiva de 10 até 1.
+
+for numero in range(10, 0, -1):
+    print(numero)
