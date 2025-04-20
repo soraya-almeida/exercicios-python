b6a6693f1081ded2bc5949ebe20b732934350c12
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-19 23:41:26 -0300

    feito

---
+++ for/exercicio16.py
@@ -0,0 +1,4 @@
+#Repita a letra “A” 6 vezes na mesma linha.
+
+for _ in range(1):
+    print("A" * 6)
---
+++ for/exercicio17.py
@@ -0,0 +1,4 @@
+#Crie um laço que diga "Linha x", onde x é o número da linha, de 1 a 10.
+
+for numero in range(11):
+    print(f"Linha {numero}")
