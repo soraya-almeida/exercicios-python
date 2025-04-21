cf253ad7d4e1216662766977149a73e23c721a10
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-04-21 19:06:36 -0300

    feito

---
+++ while/exercicio07.py
@@ -0,0 +1,8 @@
+#Dobros até ultrapassar 1000
+#Comece com o número 1 e vá dobrando ele até que o valor ultrapasse 1000. Mostre todos os valores.
+
+numero = 1
+
+while numero <= 1024:
+    print(numero)
+    numero *= 2
